4dd1ad1ee0eb970bd6aa8f8536c38b40642d23ec
Manu M S <manushivadeva2005@gmail.com>
2024-09-05 22:02:16 -0400

    first commit

--- qr_codeadvanced.py
+++ qr_codeadvanced.py
@@ -4,7 +4,7 @@ qr=qrcode.QRCode(version=1,
                  error_correction=qrcode.constants.ERROR_CORRECT_H,
                  box_size=1,border=1,)
 
-qr.add_data('https://youtu.be/FOGRHBp6lvM?si=H8t0Z40kY_c8xDb1')
+qr.add_data('https://docs.python.org/3/c-api/memory.html')
 qr.make(fit=True)
 img=qr.make_image(fill_color='red',back_color='white')
-img.save('wscube_web.png')
+img.save('qrcode.png')
